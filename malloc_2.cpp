c37927bfa15c4d66616affaccbfee87f9f16510c
Ofek <ofek.israel@campus.technion.ac.il>
2025-07-03 18:41:44 +0300

    Adding scalloc

--- malloc_2.cpp
+++ malloc_2.cpp
@@ -1,5 +1,6 @@
 #include <cstddef>
 #include <unistd.h>
+#include <limits>
 
 struct MallocMataData {
     size_t size;
@@ -8,12 +9,11 @@ struct MallocMataData {
     MallocMataData* prev;
 };
 
-MallocMataData* head = nullptr;
-void* start = sbrk(0);
+static MallocMataData* head = nullptr;
+static MallocMataData* last = nullptr;
 
 void* smalloc(size_t size) {
     MallocMataData* current = head;
-    void* current_ptr = start
     while(current != nullptr) {
         current_ptr += (void*) current->size;
         if(current->size >= size) break;
@@ -21,5 +21,62 @@ void* smalloc(size_t size) {
     if(current != nullptr) {
         
     }
+}
+
+void* scalloc(size_t num, size_t size) {
+
+    if (num == 0 || size == 0) return nullptr;
+
+    if (num > SIZE_MAX / size) return nullptr;
+
+    if (num * size > 100000000) return nullptr;
+
+    MallocMataData* current = head;
+    bool is_zero;
+
+    while(current != nullptr) {
+
+        if(current->is_free && current->size >= num * size) {
+            is_zero = true;
+            auto* data = (char*)current + sizeof(MallocMataData);
+
+            for(size_t i = 0; i < num * size; i++) {
+                if(data[i] != 0) {
+                    is_zero = false;
+                    break;
+                }
+            }
+
+            if(is_zero) {
+                current->is_free = false;
+                return data;
+            }
+        }
+
+        current = current->next;
+
+    }
+
+    void* value = sbrk(num * size + sizeof(MallocMataData));
+    if(value == (void *) -1) return nullptr;
+
+    MallocMataData* m = (MallocMataData*) value;
+    m->size = num * size;
+    m->is_free = false;
+    m->next = nullptr;
+    m->prev = last;
+
+    if (!head) {
+        head = m;
+    } else {
+        last->next = m;
+    }
+    last = m;
+
+    auto* data = (char*)m + sizeof(MallocMataData);
+    for(size_t i = 0; i < num * size; i++) {
+        data[i] = 0;
+    }
+    return data;
 }
 
